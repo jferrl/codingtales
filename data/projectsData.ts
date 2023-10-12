43ce6c2dcd0aa6ff587f92e82cce350555f84857
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2023-10-12 21:38:40 +0200

    projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,28 @@
 const projectsData = [
   {
-    title: 'test',
-    description: `for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'go-kraken',
+    description: `Go library for accessing the Kraken API 🐙.`,
+    href: 'https://github.com/jferrl/go-kraken',
+  },
+  {
+    title: 'go-merche',
+    description: `Go library for accessing the Mercedes-Benz vehicles API.`,
+    href: 'https://github.com/jferrl/go-merche',
+  },
+  {
+    title: 'stream-to-buffer',
+    description: `A promise based npm package that converts a node.js ReadStream to buffer.`,
+    href: 'https://github.com/jferrl/stream-to-buffer',
+  },
+  {
+    title: 'React testing strategies',
+    description: `A project where we are going to put into practice different strategies to test our React project.`,
+    href: 'https://github.com/jferrl/react-testing-strategies',
+  },
+  {
+    title: 'my-merche',
+    description: `Telegram bot to interact with Mercedes vehicle data.`,
+    href: 'https://github.com/jferrl/my-merche',
   },
 ]
 
